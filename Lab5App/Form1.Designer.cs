c10623c4eefae254b448f0554a29fee9189f7cd3
Eva Lena Stegmann <eva.lena.stegmann@iths.se>
2020-11-05 10:28:39 +0100

    Add project files.

---
+++ Lab5App/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Lab5App
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.listBoxImages = new System.Windows.Forms.ListBox();
+            this.labelNoOfImages = new System.Windows.Forms.Label();
+            this.labelURL = new System.Windows.Forms.Label();
+            this.buttonExtract = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.labelFoundImages = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxURL.Location = new System.Drawing.Point(15, 27);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(657, 20);
+            this.textBoxURL.TabIndex = 0;
+            // 
+            // listBoxImages
+            // 
+            this.listBoxImages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxImages.FormattingEnabled = true;
+            this.listBoxImages.Location = new System.Drawing.Point(15, 86);
+            this.listBoxImages.Name = "listBoxImages";
+            this.listBoxImages.Size = new System.Drawing.Size(773, 381);
+            this.listBoxImages.TabIndex = 1;
+            // 
+            // labelNoOfImages
+            // 
+            this.labelNoOfImages.AutoSize = true;
+            this.labelNoOfImages.Location = new System.Drawing.Point(12, 70);
+            this.labelNoOfImages.Name = "labelNoOfImages";
+            this.labelNoOfImages.Size = new System.Drawing.Size(13, 13);
+            this.labelNoOfImages.TabIndex = 2;
+            this.labelNoOfImages.Text = "0";
+            // 
+            // labelURL
+            // 
+            this.labelURL.AutoSize = true;
+            this.labelURL.Location = new System.Drawing.Point(12, 9);
+            this.labelURL.Name = "labelURL";
+            this.labelURL.Size = new System.Drawing.Size(150, 13);
+            this.labelURL.TabIndex = 3;
+            this.labelURL.Text = "Enter a URL to find its images:";
+            // 
+            // buttonExtract
+            // 
+            this.buttonExtract.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExtract.Location = new System.Drawing.Point(691, 27);
+            this.buttonExtract.Name = "buttonExtract";
+            this.buttonExtract.Size = new System.Drawing.Size(97, 23);
+            this.buttonExtract.TabIndex = 1;
+            this.buttonExtract.Text = "Extract";
+            this.buttonExtract.UseVisualStyleBackColor = true;
+            this.buttonExtract.Click += new System.EventHandler(this.buttonExtract_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Location = new System.Drawing.Point(691, 479);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(97, 23);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "Save images";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // labelFoundImages
+            // 
+            this.labelFoundImages.AutoSize = true;
+            this.labelFoundImages.Location = new System.Drawing.Point(43, 70);
+            this.labelFoundImages.Name = "labelFoundImages";
+            this.labelFoundImages.Size = new System.Drawing.Size(73, 13);
+            this.labelFoundImages.TabIndex = 6;
+            this.labelFoundImages.Text = "found images:";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.progressBar.Location = new System.Drawing.Point(15, 479);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(657, 23);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 9;
+            // 
+            // FormMain
+            // 
+            this.AcceptButton = this.buttonExtract;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 514);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.labelFoundImages);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonExtract);
+            this.Controls.Add(this.labelURL);
+            this.Controls.Add(this.labelNoOfImages);
+            this.Controls.Add(this.listBoxImages);
+            this.Controls.Add(this.textBoxURL);
+            this.Name = "FormMain";
+            this.ShowIcon = false;
+            this.Text = "ImageScraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.ListBox listBoxImages;
+        private System.Windows.Forms.Label labelNoOfImages;
+        private System.Windows.Forms.Label labelURL;
+        private System.Windows.Forms.Button buttonExtract;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label labelFoundImages;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ Lab5App/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5App
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        
+        private void buttonExtract_Click(object sender, EventArgs e)
+        {
+            labelNoOfImages.Text = "0";
+            progressBar.Value = 0;
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string url = FormatURL(textBoxURL.Text);
+                    Task<string> download = client.GetStringAsync(url);
+                    listBoxImages.Items.Clear();
+                    string regEx = "(?<=<img[^>]*src=\")([^\">]+)(?=\")";
+
+                    download.Wait();
+
+                    MatchCollection matches = Regex.Matches(download.Result, regEx, RegexOptions.IgnoreCase);
+                    foreach (var match in matches)
+                    {
+                        if (match.ToString().Contains(".bmp") || match.ToString().Contains(".png") || match.ToString().Contains(".gif") ||
+                            match.ToString().Contains(".jpg") || match.ToString().Contains(".jpeg"))
+                        {
+                            string imageURL = "";
+                            if (Uri.IsWellFormedUriString(match.ToString(), UriKind.Relative))
+                            {
+                                imageURL = url + match.ToString();
+                            }
+                            else if (Uri.IsWellFormedUriString(match.ToString(), UriKind.Absolute))
+                            {
+                                imageURL = match.ToString();
+                            }
+                            if (imageURL != "" && !listBoxImages.Items.Contains(imageURL))
+                            {
+                                listBoxImages.Items.Add(imageURL);
+                            }
+                        }
+                    }
+                    labelNoOfImages.Text = listBoxImages.Items.Count.ToString();
+                    progressBar.Maximum = listBoxImages.Items.Count;
+                }
+            }
+            catch
+            {
+                MessageBox.Show($"Could not extract the images from {textBoxURL.Text}.");
+            }
+        }
+
+        private async void buttonSave_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    progressBar.Value = 0;
+                    await DownloadImagesAsync(folderBrowserDialog.SelectedPath);
+                    MessageBox.Show("Download completed.");
+                }
+            }
+        }
+
+        private string FormatURL(string inputURL)
+        {
+            int length = inputURL.Length;
+            if (inputURL.Substring(length - 1, 1) == "/")
+            {
+                inputURL = inputURL.Substring(0, length - 1);
+            }
+
+            if (inputURL.StartsWith("http://www.") || inputURL.StartsWith("https://www."))
+            {
+                return inputURL;
+            }
+
+            if (inputURL.StartsWith("http://") || inputURL.StartsWith("https://"))
+            {
+                int pos = 7;
+                if (inputURL.StartsWith("https://"))
+                {
+                    pos++;
+                }
+                return inputURL.Substring(0, pos) + "www." + inputURL.Substring(pos);
+            }
+            else
+            {
+                if (inputURL.StartsWith("www."))
+                {
+                    return "http://" + inputURL;
+                }
+                else
+                {
+                    return "http://www." + inputURL;
+                }
+            }
+        }
+
+        private async Task DownloadImagesAsync(string selectedPath)
+        {
+            Dictionary<Task<byte[]>, string> imageDownloadTasks = new Dictionary<Task<byte[]>, string>();
+
+            for (int i = 0; i < listBoxImages.Items.Count; i++)
+            {
+                try
+                {
+                    HttpClient httpClient = new HttpClient();
+                    imageDownloadTasks.Add(httpClient.GetByteArrayAsync(listBoxImages.Items[i].ToString()), listBoxImages.Items[i].ToString());
+                }
+                catch
+                {
+                }
+            }
+
+            int j = 0;
+            while (imageDownloadTasks.Count > 0) 
+            {
+                Task<byte[]> task = null;
+                try
+                {
+                    task = await Task.WhenAny(imageDownloadTasks.Keys);
+                    if (task.IsCompleted)
+                    {
+                        _ =  SaveImageToDiscAsync(task, Path.Combine(selectedPath, $"{GetFileNameFromURL(imageDownloadTasks[task])}" +
+                                                                                   $"--{j}" +
+                                                                                   $"{GetFileExtensionFromURL(imageDownloadTasks[task])}"));
+                    }
+
+                    imageDownloadTasks.Remove(task);
+                }
+                catch
+                {
+                    imageDownloadTasks.Remove(task);
+                }
+                j++;
+                progressBar.PerformStep();
+            }
+        }
+
+        private string GetFileNameFromURL(string url)
+        {
+            url = url.Split('?')[0];
+            url = url.Split('/').Last();
+            return url.Contains('.') ? url.Substring(0,url.LastIndexOf('.')) : "";
+        }
+
+        private string GetFileExtensionFromURL(string url)
+        {
+            url = url.Split('?')[0];
+            url = url.Split('/').Last();
+            return url.Contains('.') ? url.Substring(url.LastIndexOf('.')) : "";
+        }
+
+        private async Task SaveImageToDiscAsync(Task<byte[]> image, string filename)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(filename, FileMode.Create))
+                {
+                    await fs.WriteAsync(image.Result, 0, image.Result.Length);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
